4d0f7899b4c468c82fd0aa8bbfde9c17732d5b0a
Jérémy Guillon <jeremy.damien.guillon@gmail.com>
2019-07-05 11:00:54 +0200

    Add image generator for test and training sets

--- owkin/utils.py
+++ owkin/utils.py
@@ -28,7 +28,7 @@ def load_training_features(data_dir, with_annotated=True):
     """
     assert data_dir.is_dir(), f"{data_dir} is not a directory"
     train_dir = data_dir / "train_input" / "resnet_features"
-    train_output_filename = data_dir / "train_output.csv"
+    train_output_filename = data_dir / "training_output.csv"
 
     # Load training output
     logging.debug(f'reading csv file: {train_output_filename}')
@@ -132,85 +132,97 @@ def load_test_features(data_dir):
         logging.debug(f'loading {f}')
         x_test.append(np.load(f))
 
+    return x_test, test_output
 
-def load_resnet_features(data_dir):
-    """Load the ResNet50 feature vectors of all the annotated tiles with their
-    corresponding target.
 
-    Args:
-        data_dir: directory where the predefined data tree is stored
+def _load_image_generator(batch_size, target_size, image_dir, preprocessing_function=None, with_annotated=True):
+    """
+    """
+    from keras_preprocessing.image import \
+        ImageDataGenerator  # custom fix from some guy on github
+    
+    # Collect the images filename
+    logging.info(f"listing {image_dir}...")
+    filenames_df = pd.DataFrame(columns=['ID', 'Filename'])
+    subject_dirs = sorted(image_dir.glob("ID_*"))
+    if not with_annotated:
+        subject_dirs = [d for d in subject_dirs if d.stem[6:]!='_annotated']
+    logging.info(f"the folder contains {len(subject_dirs)} subjects")
+    assert len(subject_dirs) > 0, f"folder {image_dir} is empty"
+    for subject_dir in subject_dirs:
+        if subject_dir.is_dir():
+            subject_id = subject_dir.stem[3:]
+            logging.info(f"  extracting subjects {subject_id}'s image filenames")
+            subject_images = sorted(subject_dir.glob("*.jpg"))
+            for image in subject_images:
+                assert image.is_file(), f"{image} is not a file"
+                logging.debug(f"    {image}")
+                filenames_df = filenames_df.append({
+                    'ID'      : subject_id,
+                    'Filename': 'ID_' + subject_id + '/' + image.stem + image.suffix
+                }, ignore_index=True)
 
-    Returns:
-        x_train: n x p array, where n is the number of tiles and p the number of
-            features.
-        y_train: n x 1 vector, containing zeros for normal, ones for metastatic.
+    # Create the image generator
+    image_generator = ImageDataGenerator(
+            preprocessing_function=preprocessing_function)
+    image_iterator = image_generator.flow_from_dataframe(
+            dataframe=filenames_df, x_col='Filename',
+            directory=image_dir,
+            target_size=target_size,
+            batch_size=batch_size,
+            shuffle=False,
+            class_mode=None
+    )
 
+    return image_iterator, filenames_df
+
+
+def load_training_images(batch_size, target_size, data_dir=Path('data/'),
+                     preprocessing_function=None, with_annotated=True):
     """
 
-    # Load the data
-    assert data_dir.is_dir(), f"{data_dir} is not a directory"
-
-    train_dir = data_dir / "train_input" / "resnet_features"
-    test_dir = data_dir / "test_input" / "resnet_features"
-    train_tiles_annot_filename = data_dir / "train_input" / \
-                                 "train_tile_annotations.csv"
-    train_output_filename = data_dir / "train_output.csv"
-
-    logging.debug(f'reading csv file: {train_tiles_annot_filename}')
-    train_tiles_annot = pd.read_csv(train_tiles_annot_filename)
-
-    # Get the labels
-    y_train = train_tiles_annot["Target"].values
-
-    # Get the filenames
-    train_ids_annot = train_tiles_annot.iloc[:, 0].str[:6].unique()
-    filenames_train = [train_dir / f"{idx}_annotated.npy" for idx in
-                       train_ids_annot]
-    for filename in filenames_train:
-        assert filename.is_file(), f"{filename} is not a file"
-
-    # Load numpy arrays
-    features = []
-    for f in filenames_train:
-        logging.debug(f'loading {f}')
-        patient_features = np.load(f)
-        features.append(patient_features)
-
-    x_train = np.concatenate(features)
-    assert len(x_train) == len(y_train)
+    """
+    
+    train_images_dir = data_dir / "train_input" / "images"
+    
+    train_image_iterator, train_filenames_df = _load_image_generator(
+        image_dir=train_images_dir,
+        batch_size=batch_size,
+        target_size=target_size,
+        preprocessing_function=preprocessing_function,
+        with_annotated=with_annotated
+    )
+    
+    train_output_filename = data_dir / "training_output.csv"
 
     # Load training output
     logging.debug(f'reading csv file: {train_output_filename}')
     train_output = pd.read_csv(train_output_filename)
+    train_output = train_output.set_index('ID')
+    
+    # Remove annotated subjects
+    if not with_annotated:
+        ids = train_filenames_df["ID"].str[:3].unique()
+        train_output = train_output[train_output['ID'].isin(ids)]
+    
+    return train_image_iterator, train_filenames_df, train_output
+    
 
-    # Load training data
-    train_output_na = train_output[train_output["ID"].str[-9:] != 'annotated']
-    filenames_train_na = [train_dir / f"{idx}.npy" for idx in
-                          train_output_na["ID"]]
-    for filename in filenames_train_na:
-        assert filename.is_file(), f"{filename} is not a file"
+def load_test_images(batch_size, target_size, data_dir=Path('data/'),
+                     preprocessing_function=None):
+    """
 
-    # Load numpy arrays
-    x_train_na = []
-    for f in filenames_train_na:
-        logging.debug(f'loading {f}')
-        patient_features = np.load(f)
-        x_train_na.append(patient_features)
-
-    return x_train, y_train, x_train_na, train_output_na
-
-
-def load_training_images():
-    pass
-
-
-def load_test_images():
-    pass
-
+    """
+    
+    return _load_image_generator(
+        image_dir=data_dir / "test_input" / "images",
+        batch_size=batch_size,
+        target_size=target_size,
+        preprocessing_function=preprocessing_function,
+    )
+    
 
 def submit_test_output(test_output, file_id, output_dir=Path('outputs/')):
-    return x_test, test_output
-
     """Submit the test output by simply writing it to a .csv file.
 
     Args:
